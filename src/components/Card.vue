1bc8ade436670132b9ff3a15f70e8be42d82a248
dya-code <entrydia7691@gmail.com>
2022-10-14 23:13:33 +0900

    front design

---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card p-5 m-10 rounded-lg w-[500px] h-[200px] bg-neutral-800 flex items-center justify-center">
+    <img class="w-40 h-40 object-cover rounded-lg mr-5" src="../assets/backbg.png" />
+    <div class="ml-2">
+      <div class="title text-neutral-200 mb-2 text-lg">"프론트 노동" 열일하는 개발자 디아</div>
+      <div class="content text-neutral-500">개발자인 디아는 스네 위키 개발을 위해 프론트 개발을 하고 있습니다, 개발자인 디아는 스네 위키 개발을 위해 프론트 개발을 하고 있습니다</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Card'
+}
+</script>
+
+<style scoped>
+  .content {
+    /* 한 줄 자르기 */
+    display: inline-block;
+    width: 280px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    /* 여러 줄 자르기 추가 스타일 */
+    white-space: normal;
+    line-height: 1.2;
+    height: 3.6em;
+    text-align: left;
+    word-wrap: break-word;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+  }
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <Nav />
+  
+  <div class="cards p-5 flex justify-center flex-wrap">
+    <Card v-for="item in 6" />
+  </div>
+
+</template>
+
+<script>
+import Nav from '../components/Nav.vue'
+import Card from '../components/Card.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    Nav,
+    Card
+  }
+}
+</script>
+
+<style>
+</style>
