c665ca36df19c4f266d9bdc356210ecd25354036
dya-code <entrydia7691@gmail.com>
2022-10-14 23:17:28 +0900

    name SKW to KDW

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar font-mono flex justify-between items-center p-4 rounded-full m-10 shadow-2xl bg-neutral-800">
-    <nav-title class="font-bold text-4xl ml-20 text-neutral-300">SkW</nav-title>
+    <nav-title class="font-bold text-4xl ml-20 text-neutral-300">KDW</nav-title>
     <nav-item class="font-bold text-2xl mr-20 w-8 brightness-75 hover:brightness-100"><img src="../assets/sign.png" /></nav-item>
   </nav>
 </template>
