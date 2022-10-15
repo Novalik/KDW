1d28c8046c22ccbc0e197533816265acb1668605
hooss-only <hooss-only@github.com>
2022-10-15 15:54:07 +0900

    main page

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,7 @@
 <template>
   <header>
-    Discord News
+    <span>Discord News</span>
+    <font-awesome-icon icon="magnifying-glass" />
   </header>
 </template>
 
@@ -14,13 +15,12 @@ export default {
 header {
     font-family: 'DM Serif Display', serif;
     backdrop-filter: saturate(200%) blur(10px);
-    text-align: center;
     background-color: rgba(255, 255, 255, 0.8);
     font-weight: 100;
     top: 0;
     position: sticky;
     z-index: 100;
-    padding: 10px;
+    padding: 10px 30%;
     border-bottom-style: solid;
     border-bottom-color: rgba(0, 0, 0, 0.171);
     border-bottom-width: 1px;
